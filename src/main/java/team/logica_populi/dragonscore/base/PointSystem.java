a16320403b1e3568e2d9552e9e721a3d469116b1
Alex <alexdollar3@gmail.com>
2025-03-27 10:04:56 -0500

    commented out old code

--- src/main/java/team/logica_populi/dragonscore/base/PointSystem.java
+++ src/main/java/team/logica_populi/dragonscore/base/PointSystem.java
@@ -1,7 +1,5 @@
 package team.logica_populi.dragonscore.base;
 
-import org.json.JSONObject;
-
 import java.util.logging.Logger;
 
 public class PointSystem {
@@ -34,74 +32,74 @@ public class PointSystem {
 
     public int getCurrent_points() {return current_points;}
 
-    /**
-     * Set the total points for the lesson
-     * @param object get the currents points stored
-     * @return new total points inside the lesson
-     */
-    public int setPoints(JSONObject object){
-        String id = object.getString("id");
-
-        if(id == null || id.isEmpty()){
-            logger.warning("Failed to load point system. id is empty or null");
-            return 0;
-        }
-
-        int points = object.getInt("total_points");
-        int newPoints = points + this.selected_points;
-
-        object.put("total_points", newPoints);
-        current_points = newPoints;
-
-        return current_points;
-    }
-
-    /**
-     * Gets the current total points in the lesson
-     * @param object the object to load
-     * @return the loaded data or returns 0
-     */
-    public int getTotalPoints(JSONObject object) {
-        String id = object.getString("id");
-
-        if(id == null || id.isEmpty()){
-            logger.warning("Failed to load point system. id is empty or null");
-            return 0;
-        }
-
-        this.current_points = object.getInt("total_points");
-
-        return current_points;
-    }
-
-
-    /**
-     * Hash or encrypt the point
-     */
-    private static String encrypt_points(){
-        return "0";
-    }
-
-    /**
-     * Load the Point system from JSON object.
-     * @param object the object to load from
-     * @return the loaded data or returns null if something fails
-     */
-    public static PointSystem loadFromJSON(JSONObject object){
-        String id = object.getString("id");
-        if(id == null || id.isEmpty()){
-            logger.warning("Failed to load point system. id is empty or null");
-            return null;
-        }
-
-        String name = object.getString("name");
-        if(name == null)  name = "";
-
-        int current_points = object.getInt("total_points");
-
-        return new PointSystem(id, name, 0, current_points);
-
-    }
+//    /**
+//     * Set the total points for the lesson
+//     * @param object get the currents points stored
+//     * @return new total points inside the lesson
+//     */
+//    public int setPoints(JSONObject object){
+//        String id = object.getString("id");
+//
+//        if(id == null || id.isEmpty()){
+//            logger.warning("Failed to load point system. id is empty or null");
+//            return 0;
+//        }
+//
+//        int points = object.getInt("total_points");
+//        int newPoints = points + this.selected_points;
+//
+//        object.put("total_points", newPoints);
+//        current_points = newPoints;
+//
+//        return current_points;
+//    }
+//
+//    /**
+//     * Gets the current total points in the lesson
+//     * @param object the object to load
+//     * @return the loaded data or returns 0
+//     */
+//    public int getTotalPoints(JSONObject object) {
+//        String id = object.getString("id");
+//
+//        if(id == null || id.isEmpty()){
+//            logger.warning("Failed to load point system. id is empty or null");
+//            return 0;
+//        }
+//
+//        this.current_points = object.getInt("total_points");
+//
+//        return current_points;
+//    }
+//
+//
+//    /**
+//     * Hash or encrypt the point
+//     */
+//    private static String encrypt_points(){
+//        return "0";
+//    }
+//
+//    /**
+//     * Load the Point system from JSON object.
+//     * @param object the object to load from
+//     * @return the loaded data or returns null if something fails
+//     */
+//    public static PointSystem loadFromJSON(JSONObject object){
+//        String id = object.getString("id");
+//        if(id == null || id.isEmpty()){
+//            logger.warning("Failed to load point system. id is empty or null");
+//            return null;
+//        }
+//
+//        String name = object.getString("name");
+//        if(name == null)  name = "";
+//
+//        int current_points = object.getInt("total_points");
+//
+//        return new PointSystem(id, name, 0, current_points);
+//
+//    }
 
     @Override
     public String toString() {
