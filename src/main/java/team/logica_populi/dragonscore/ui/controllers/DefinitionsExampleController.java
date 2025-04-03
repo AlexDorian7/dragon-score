be1bdd1ac4bb56c59db0c544ff04109c8eb842da
Lance Cole <rebelart084@gmail.com>
2025-04-02 23:38:59 -0500

    Fixed Mouse Event Error

--- src/main/java/team/logica_populi/dragonscore/ui/controllers/DefinitionsExampleController.java
+++ src/main/java/team/logica_populi/dragonscore/ui/controllers/DefinitionsExampleController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import team.logica_populi.dragonscore.base.logic.Answer;
 import team.logica_populi.dragonscore.base.logic.Question;
