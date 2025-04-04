10f588e9a86634d5382ac8cb4199efd4f6493b66
Njones36 <nickrjones36@gmail.com>
2025-04-03 22:54:54 -0500

    ///////////////////// added color change to some buttons and deselect all on submit. Also kinda fixed padding on the answer box and progress bar.

--- src/main/java/team/logica_populi/dragonscore/ui/controllers/DefinitionsExampleController.java
+++ src/main/java/team/logica_populi/dragonscore/ui/controllers/DefinitionsExampleController.java
@@ -83,6 +83,7 @@ public class DefinitionsExampleController {
      */
     @FXML
     public void onSubmit(ActionEvent actionEvent) {
+        deselectAll();
         if (callback != null) {
             callback.accept(new ArrayList<>()); // TODO: Actually send back the selected answers
         }
@@ -90,12 +91,19 @@ public class DefinitionsExampleController {
 
     private void deselectAll() {
         answer1.setSelected(false);
+        answer1.setStyle(String.format("-fx-background-color: #fce6a4;", answer1.getStyle()));
         answer2.setSelected(false);
+        answer2.setStyle(String.format("-fx-background-color: #fce6a4;", answer2.getStyle()));
         answer3.setSelected(false);
+        answer3.setStyle(String.format("-fx-background-color: #fce6a4;", answer3.getStyle()));
         answer4.setSelected(false);
+        answer4.setStyle(String.format("-fx-background-color: #fce6a4;", answer4.getStyle()));
         answer5.setSelected(false);
+        answer5.setStyle(String.format("-fx-background-color: #fce6a4;", answer5.getStyle()));
         answer6.setSelected(false);
+        answer6.setStyle(String.format("-fx-background-color: #fce6a4;", answer6.getStyle()));
         answer7.setSelected(false);
+        answer7.setStyle(String.format("-fx-background-color: #fce6a4;", answer7.getStyle()));
     }
 
     public void selectAnswer(ActionEvent event) {
@@ -103,5 +111,6 @@ public class DefinitionsExampleController {
         ToggleButton clickedButton = (ToggleButton) event.getSource();
         clickedButton.setSelected(true);
         questionArea.setText("Selected answer: " + clickedButton.getText());
+        clickedButton.setStyle(String.format("-fx-background-color: #C2B280;", clickedButton.getStyle()));
     }
 }
