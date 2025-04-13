1203395d09b81a5817e06a2f576b46da19786044
Eric Hansson <erichansson.united@gmail.com>
2025-04-12 19:22:52 -0500

    Working on integrating the submission code to the project

--- src/main/java/team/logica_populi/dragonscore/ui/controllers/SubmissionCodeController.java
+++ src/main/java/team/logica_populi/dragonscore/ui/controllers/SubmissionCodeController.java
@@ -1,4 +1,20 @@
 package team.logica_populi.dragonscore.ui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+import team.logica_populi.dragonscore.base.points.SubmissonCode;
+
 public class SubmissionCodeController {
+    @FXML
+    private WebView submissionCodeArea;
+
+    private SubmissonCode code;
+
+    @FXML
+    public void initialize(){
+    }
+
+
+
+
 }
