5175c83a4e665386e1d71b2fc2de9aef67a4828c
Alex <alexdollar3@gmail.com>
2025-04-24 11:24:12 -0500

    Merged and made some small changes

---
+++ src/main/java/team/logica_populi/dragonscore/ui/controllers/TruthTableQuestionForm.java
@@ -0,0 +1,198 @@
+package team.logica_populi.dragonscore.ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.web.WebView;
+import team.logica_populi.dragonscore.base.logic.Answer;
+import team.logica_populi.dragonscore.base.logic.Question;
+import team.logica_populi.dragonscore.base.registries.DragonHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class TruthTableQuestionForm {
+    @FXML
+    private Button HomeMenu;
+    @FXML
+    private WebView questionArea;
+    @FXML
+    private FlowPane answerArea;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private ToggleButton easyDif;
+    @FXML
+    private ToggleButton normDif;
+    @FXML
+    private ToggleButton hardDif;
+    @FXML
+    private ProgressBar progressBar;
+
+    private Consumer<List<Answer>> callback;
+    private Runnable nextQuestionCallback;
+    private Question question;
+    private ArrayList<RadioButton> answerButtons = new ArrayList<>();
+
+    private boolean resultsShown = false;
+
+    // Initialize the controller
+    @FXML
+    public void initialize() {
+        ToggleGroup difficultyGroup = new ToggleGroup();
+        easyDif.setToggleGroup(difficultyGroup);
+        normDif.setToggleGroup(difficultyGroup);
+        hardDif.setToggleGroup(difficultyGroup);
+
+        setDifficultlyHandler(easyDif);
+        setDifficultlyHandler(normDif);
+        setDifficultlyHandler(hardDif);
+        updateDifficultyStyles();
+    }
+    private void setDifficultlyHandler(ToggleButton button) {
+        button.setOnAction(event -> {
+
+            ToggleButton clickedButton = (ToggleButton) event.getSource();
+            int difficulty = Integer.parseInt(clickedButton.getText());
+
+            updateDifficultyStyles();
+
+            if (DragonHandler.getCurrentSession() != null) {
+                DragonHandler.getCurrentSession().setPointsToGive(difficulty);
+            }
+        });
+    }
+    private void updateDifficultyStyles() {
+        ToggleButton[] buttons = { easyDif, normDif, hardDif };
+
+        for (ToggleButton btn : buttons) {
+            if (btn.isSelected()) {
+                btn.setStyle("-fx-background-radius: 100; -fx-background-color: #645c41; -fx-text-fill: black; -fx-pref-width: 50; -fx-pref-height: 50; -fx-font-size: 1.1em;"); // green
+            } else {
+                btn.setStyle("-fx-background-radius: 100; -fx-background-color: #fce6a4; -fx-text-fill: black; -fx-pref-width: 50; -fx-pref-height: 50; -fx-font-size: 1.1em;"); // green
+            }
+        }
+    }
+
+
+    @FXML
+    private void onDifficultySelect(ActionEvent event) {
+        ToggleButton clickedButton = (ToggleButton) event.getSource();
+        int difficulty = Integer.parseInt(clickedButton.getText());
+
+        if (DragonHandler.getCurrentSession() != null) {
+            DragonHandler.getCurrentSession().setPointsToGive(difficulty);
+        }
+    }
+
+    /**
+     * Sets the question that this view is to display.
+     *
+     * @param question The question to display
+     */
+    public void setQuestion(Question question) {
+        this.question = question;
+        resultsShown = false;
+        questionArea.getEngine().loadContent("<html><head></head><body style='font-size:1.5em;'>" + question.getQuestion() + "</body></html>");
+        answerButtons.clear();
+        answerArea.getChildren().clear();
+        for (Answer answer : question.getAnswers()) {
+            RadioButton button = new RadioButton(answer.getText());
+            answerArea.getChildren().add(button);
+            answerButtons.add(button);
+            button.setOnAction(this::selectAnswer);
+        }
+        submitButton.setText("Submit");
+    }
+
+    /**
+     * Sets the progress bar value.
+     *
+     * @param progress The value to display in the progress bar
+     */
+    public void setProgress(double progress) {
+        if (progress > 1) progress = 1;
+        if (progress < 0) progress = 0;
+        progressBar.setProgress(progress);
+    }
+
+    /**
+     * Shows the correct answer(s) to the user and prevents any additional answers from being recorded.
+     */
+    public void showCorrect() {
+        resultsShown = true;
+        for (int i = 0; i < answerButtons.size(); i++) {
+            if (answerButtons.get(i).isSelected()) {
+                answerButtons.get(i).setStyle("-fx-background-color: #FF7770");
+            }
+            if (question.getAnswers().get(i).isCorrect()) {
+                answerButtons.get(i).setStyle("-fx-background-color: #9FD4A3");
+            }
+        }
+        submitButton.setText("Next Question");
+    }
+
+    /**
+     * Sets the on submitted callback consumer.
+     *
+     * @param callback The consumer to call with the selected answers
+     */
+    public void setSubmitCallback(Consumer<List<Answer>> callback) {
+        this.callback = callback;
+    }
+
+    /**
+     * Called when the submit button is pressed.
+     *
+     * @param actionEvent Event details provided by JavaFX
+     */
+    @FXML
+    public void onSubmit(ActionEvent actionEvent) {
+        if (resultsShown) {
+            if (nextQuestionCallback != null)
+                nextQuestionCallback.run();
+            return;
+        }
+        if (callback != null) {
+            ArrayList<Answer> answers = new ArrayList<>();
+            for (int i = 0; i < answerButtons.size(); i++) {
+                if (answerButtons.get(i).isSelected()) {
+                    answers.add(question.getAnswers().get(i));
+                }
+            }
+            if (answers.isEmpty()) return; // The user did not select an answer. Do nothing.
+            callback.accept(answers);
+        }
+    }
+
+    private void deselectAll() {
+        for (RadioButton answerButton : answerButtons) {
+            answerButton.setSelected(false);
+        }
+
+    }
+
+    private void selectAnswer(ActionEvent event) {
+        deselectAll();
+        RadioButton clickedButton = (RadioButton) event.getSource();
+        clickedButton.setSelected(true);
+    }
+
+    /**
+     * Sets the callback that will be called when the user wants to go to the next question.
+     *
+     * @param nextQuestionCallback The callback to be executed
+     */
+    public void setNextQuestionCallback(Runnable nextQuestionCallback) {
+        this.nextQuestionCallback = nextQuestionCallback;
+    }
+
+    @FXML
+    private void goHome(ActionEvent actionEvent) {
+        DragonHandler currentSession = DragonHandler.getCurrentSession();
+        if (currentSession == null) return; //This should never be possible, but do nothing id it is
+        currentSession.showMainMenu(); // return to main menu. Any unsaved progress will be lost, but program saves after each submit
+    }
+}
